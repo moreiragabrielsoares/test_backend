c5571d71ca9468873153df01d82139c18f05693a
Gabriel Soares Moreira <moreira.gabrielsoares@gmail.com>
2023-02-09 18:40:20 -0300

    feat: add denylistOfCpfsRouter and errorHandler to app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
+import { errorHandler } from './middlewares/errorHandlerMiddleware';
+import { denylistOfCpfsRouter } from './routes/denylistOfCpfsRoute';
 
 const app = express();
 
 app.use(cors());
 app.use(json());
+app.use(denylistOfCpfsRouter);
+app.use(errorHandler);
 
 export { app };
