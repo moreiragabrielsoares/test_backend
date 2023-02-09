a3eab57812fd269721199596a5fb75ed8330e997
Gabriel Soares Moreira <moreira.gabrielsoares@gmail.com>
2023-02-09 18:38:41 -0300

    feat: create denylistOfCpfsService

---
+++ src/services/denylistOfCpfsService.ts
@@ -0,0 +1,40 @@
+import { IAddNewCpfToDenylist } from 'types/denylistOfCpfsTypes';
+import * as denylistOfCpfsRepository from '../repositories/denylistOfCpfsRepository';
+
+async function registerNewDeniedCpf(newDeniedCpf: IAddNewCpfToDenylist) {
+  const deniedCpf = await denylistOfCpfsRepository.getDeniedCpfByCpf(newDeniedCpf.cpf);
+
+  if (deniedCpf) {
+    throw { type: 'ExistsCpfException', message: 'This cpf is already denied' };
+  }
+
+  await denylistOfCpfsRepository.registerNewDeniedCpf(newDeniedCpf);
+}
+
+async function getDeniedCpfByCpf(deniedCpf: string) {
+  const cpf = await denylistOfCpfsRepository.getDeniedCpfByCpf(deniedCpf);
+
+  if (!cpf) {
+    throw { type: 'NotFoundCpfException', message: 'This cpf is not denied' };
+  }
+
+  return cpf;
+}
+
+async function deleteDeniedCpfbyCpf(deniedCpf: string) {
+  const cpf = await denylistOfCpfsRepository.getDeniedCpfByCpf(deniedCpf);
+
+  if (!cpf) {
+    throw { type: 'NotFoundCpfException', message: 'This cpf is not denied' };
+  }
+
+  await denylistOfCpfsRepository.deleteDeniedCpfbyCpf(deniedCpf);
+}
+
+async function getAllDeniedCpfByCpf() {
+  const cpfs = await denylistOfCpfsRepository.getAllDeniedCpfs();
+
+  return cpfs;
+}
+
+export { registerNewDeniedCpf, getDeniedCpfByCpf, deleteDeniedCpfbyCpf, getAllDeniedCpfByCpf };
